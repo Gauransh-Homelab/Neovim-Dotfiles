050e8ec417455f7790cb873727131bb0b7b9f531
Gauransh Mathur <gauranshmathur1999@gmail.com>
2025-07-04 01:19:11 +0800

    allow arrow in insert mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Arrow key navigation in insert mode only
+vim.keymap.set('i', '<Left>', '<Left>', { desc = 'Move left in insert mode' })
+vim.keymap.set('i', '<Down>', '<Down>', { desc = 'Move down in insert mode' })
+vim.keymap.set('i', '<Up>', '<Up>', { desc = 'Move up in insert mode' })
+vim.keymap.set('i', '<Right>', '<Right>', { desc = 'Move right in insert mode' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
