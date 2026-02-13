6a6357be89709273c3788fdfe4a48ec008d2b928
Gauransh Mathur <gauranshmathur1999@gmail.com>
2026-02-14 02:21:07 +0800

    chore(lazy): check for updates

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -86,6 +86,10 @@ require('lazy').setup({
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
+  checker = {
+    enabled = true,
+    notify = true,
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
